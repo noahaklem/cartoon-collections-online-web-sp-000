b44771831e09c07518004aa28130197ace8e533d
Noah  Klem <klem.noah@gmail.com>
2021-05-29 18:21:36 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,6 +1,6 @@
 def roll_call_dwarves(array)
-  array.each_with_index(1) do |name, index|
-    puts "#{index} #{name}"
+  array.each_with_index do |name, index|
+    puts "#{index+1} #{name}"
   end
 end
 
