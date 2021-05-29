41ef563438ee4928ae8348a83f768c57ecdc92f0
Noah  Klem <klem.noah@gmail.com>
2021-05-29 19:09:00 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -19,6 +19,7 @@ end
 def find_the_cheese(array)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
-  
-  return cheese_types[index]
+  cheese_types.find do |type|
+    type
+
 end
