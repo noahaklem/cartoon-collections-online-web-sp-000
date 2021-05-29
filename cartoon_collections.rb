70652a0fa0b23f13668da42a2ae988975081154e
Noah  Klem <klem.noah@gmail.com>
2021-05-29 18:52:30 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,9 +13,7 @@ def summon_captain_planet(array)
 end
 
 def long_planeteer_calls(array)
-  array.all?(false) do |words| 
-    words.length > 4
-  end
+  array.all? {|words| words.length > 4}
 end
 
 def find_the_cheese# code an argument here
