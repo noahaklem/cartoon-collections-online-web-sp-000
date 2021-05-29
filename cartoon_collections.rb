b0216821846d7e1a68f1d88f70fae701bcce95b9
Noah  Klem <klem.noah@gmail.com>
2021-05-29 19:23:22 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -21,6 +21,6 @@ def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
   array.find do |index|
     cheese_types.map {|cheese| cheese == index}
-    cheese_type.first
+  
   end
 end
